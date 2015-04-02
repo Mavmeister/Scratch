8164e3b7ee2dc692c08dd1fbda447e4fbab240e5
James Maveety <jimbobthedrummer@gmail.com>
2015-04-02 13:03:26 -0700

    First commit

---
+++ scratch.js
@@ -0,0 +1,45 @@
+// This is a nonsensical creation of a function parsing the results of another function
+// to look for a string. 
+var myFunc = function(){
+  var randomNum = (Math.random());
+
+var oTT = ((randomNum * 100)+1);
+var myArr = [];
+for (var i = 0; myArr.length < 1; i++ ){
+  myArr.push(Math.ceil(oTT));
+}
+  var rightnow = new Date();
+    var dayNames = [
+        "Sun", "Mon", "Tues", "Wed",'Thurs', 'fri', 'sat']
+    var theDay = rightnow.getDay();
+    var full = ("Your magic number is "+myArr+", and the day is "+ dayNames[theDay] );}
+
+var Bob = myFunc.toString();
+
+var Looking = function(senty){
+  var testIndex = senty.indexOf("Thurs");
+  if (testIndex !== -1){
+    alert("THURSDAY");
+  }
+    }
+
+Looking(Bob);
+//
+
+
+
+
+
+
+
+
+
+
+
+
+           
+           
+           
+           
+           
+
