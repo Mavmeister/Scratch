8e505021f32eacba64dcd266012aad9b3c010ce1
James Maveety <jimbobthedrummer@gmail.com>
2015-04-02 13:32:59 -0700

    more random generators

--- scratch.js
+++ scratch.js
@@ -1,29 +1,63 @@
 // This is a nonsensical creation of a function parsing the results of another function
 // to look for a string. 
-var myFunc = function(){
-  var randomNum = (Math.random());
+var randomNum, theDay, rightnow 
 
-var oTT = ((randomNum * 100)+1);
-var myArr = [];
-for (var i = 0; myArr.length < 1; i++ ){
-  myArr.push(Math.ceil(oTT));
+randomNum = (Math.random());
+rightnow = new Date();
+
+var myFunc = function(){
+  var oTT = ((randomNum * 100)+1);
+  var myArr = [];
+  for (var i = 0; myArr.length < 1; i++ ){
+    myArr.push(Math.ceil(oTT));
 }
-  var rightnow = new Date();
+    theDay = rightnow.getDay();
     var dayNames = [
-        "Sun", "Mon", "Tues", "Wed",'Thurs', 'fri', 'sat']
-    var theDay = rightnow.getDay();
+        "Sun", "Mon", "Tues",         "Wed",               'Thurs', 'fri', 'sat']
+
     var full = ("Your magic number is "+myArr+", and the day is "+ dayNames[theDay] );}
 
-var Bob = myFunc.toString();
 
+var ranDay = (Math.floor(randomNum * 6) + 1);
+var aDay   = rightnow.getDay();
+
+happyDay = function(){
+  if (aDay === ranDay){
+    console.log("WOOO! HIT THE JACKPOT!")
+  }
+  else
+    {console.log("NO");} 
+  };
+
+var Bob = myFunc.toString();
 var Looking = function(senty){
   var testIndex = senty.indexOf("Thurs");
   if (testIndex !== -1){
-    alert("THURSDAY");
+    console.log("THURSDAY");
   }
     }
-
+happyDay();        
 Looking(Bob);
+
+
+
+
+
+
+
+
+
+
+
+
+
+           
+           
+           
+           
+           
+
+
 //
 
 
